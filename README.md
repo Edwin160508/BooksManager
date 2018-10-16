faa1fe33ddee5a07c1dc9a554e4feb4cfcef6251
Edwin Lima <edwin.limaa@gmail.com>
2018-10-16 15:53:25 -0300

    Inicio do Projeto.

---
+++ README.md
@@ -0,0 +1,8 @@
+# BooksManager
+BooksManager is an API of Books Management, where are add Books, authors, and price in a Data Base.  
+MySQL is the Data Base used by this API.
+
+# Technique description: 
+This API is Maven Format Project.  
+Spring Boot Version 2.0.5  
+Java Version 8
---
+++ src/main/java/com/booksmanager/api/BooksManagerApplication.java
@@ -0,0 +1,12 @@
+package com.booksmanager.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksManagerApplication.class, args);
+	}
+}
