6edb27968ef41f4a3658622ffd7455cd7da62fe7
Edwin Lima <edwin.limaa@gmail.com>
2018-10-16 20:26:28 -0300

    Importando dependencias JPA, mysql, h2. teste pegar valor de um properties exibindo seus valores no bean

--- src/main/java/com/booksmanager/api/BooksManagerApplication.java
+++ src/main/java/com/booksmanager/api/BooksManagerApplication.java
@@ -1,12 +1,24 @@
 package com.booksmanager.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BooksManagerApplication {
-
+	@Value("${peginacao.qtd_por_pagina}")
+	private int qtdPagina;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BooksManagerApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("### Quantidade de elementos por página - " + this.qtdPagina);
+		};
 	}
 }
