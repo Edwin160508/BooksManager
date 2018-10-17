0dd3eb9520deed04394eb6186c418b8d15cf0ca2
Edwin Lima <edwin.limaa@gmail.com>
2018-10-16 22:18:42 -0300

    Utilizando JUnit para teste PassworUtils validando senhas.

--- src/test/java/com/booksmanager/api/BooksManagerApplicationTests.java
+++ src/test/java/com/booksmanager/api/BooksManagerApplicationTests.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.booksmanager.api.utils.PasswordUtils;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
@@ -24,6 +26,26 @@ public class BooksManagerApplicationTests {
 	@Test
 	public void testCarregaContextoTeste() {
 		assertEquals(300, this.qtdPagina);
+	}
+	
+	/**
+	 * Descrição: Teste de validação usando pacote spring Security "BCryptor"
+	 * 
+	 * Objetivo: Testar vários Hahs gerados pelo "BCryptor" validado com a mesma senha, deve
+	 * retornar true para passar no teste. 
+	 *   
+	 */
+	@Test
+	public void testValidatePassword() {
+		String encodedPassword = PasswordUtils.generatePasswordBCrypt("123456");
+		System.out.println("Senha codificada: "+encodedPassword);
+		
+		encodedPassword = PasswordUtils.generatePasswordBCrypt("123456");
+		System.out.println("Senha codificada novamente: "+encodedPassword);
+		
+		System.out.println("Senha válida: "+ PasswordUtils.validatePassword("123456", encodedPassword));
+		
+		assertEquals(true, PasswordUtils.validatePassword("123456", encodedPassword));
 	}
 
 }
